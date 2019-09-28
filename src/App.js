371edaf70f990767c2edcf3c9c822e69779cdfde
christiancebrero <chris.cebrero@gmail.com>
2019-09-27 19:38:40 -0700

    added initial rank

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
 
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
 
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div className="ma4 mt0">
+            <div>
+                <div className="white f3">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
